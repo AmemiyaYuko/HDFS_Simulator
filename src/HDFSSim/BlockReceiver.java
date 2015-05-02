69d6d1aa1972331314405caafbe9993c3a9aab87
AmemiyaYuko <kyd_dong@hotmail.com>
2015-05-02 19:07:30 +0800

    implementing tracer...

--- src/HDFSSim/BlockReceiver.java
+++ src/HDFSSim/BlockReceiver.java
@@ -14,6 +14,8 @@ public class BlockReceiver extends GridSim {
     private String entityName;
     private String nodeName;
     private DataNode dataNode;
+    private byte[] data;
+    private boolean received=false;
     BlockReceiver(String entityName,double baud_rate,DataNode dataNode) throws Exception{
         super(new String(entityName),baud_rate);
         this.nodeName=nodeName;
@@ -24,10 +26,13 @@ public class BlockReceiver extends GridSim {
         Sim_event ev=new Sim_event();
         for (sim_get_next(ev);ev.get_tag()!= GridSimTags.END_OF_SIMULATION;sim_get_next(ev)){
             if (ev.get_tag()==HDFSSimTags.READ_REQUEST){
-
+                this.received=true;
             }
             //Add gridlet message there
             super.send(ev.get_src(), GridSimTags.SCHEDULE_NOW,GridSimTags.END_OF_SIMULATION);
         }
+    }
+    public boolean hasReceived(){
+        return received;
     }
 }
