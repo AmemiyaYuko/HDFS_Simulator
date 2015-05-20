720a54fe57677fea37ed50a6627c9a3956c99216
AmemiyaYuko <kyd_dong@hotmail.com>
2015-05-21 02:44:07 +0800

    implementing write new file operation

---
+++ src/hdfssim/WriteNewFileRequest.java
@@ -0,0 +1,21 @@
+package hdfssim;
+
+/**
+ * Created by Amemiya on 5/21/15.
+ */
+public class WriteNewFileRequest {
+    private String fileName;
+    private double size;
+    public WriteNewFileRequest(String fileName, double size){
+        this.fileName=fileName;
+        this.size=size;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public double getSize() {
+        return size;
+    }
+}
